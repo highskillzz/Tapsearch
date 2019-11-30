422c814f57982bdb85c81f88cbbb69d46efc0250
highskillzz <saivicky2015@gmail.com>
2019-12-01 01:04:54 +0530

    Completed first phase backend and added the frontend build files

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from routes import admin
-from flask import Flask
 import os
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'youwontguess'
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='./build')
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.static_folder = 'static'
 app.register_blueprint(admin)
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 
 if __name__ == "__main__":
---
+++ build/precache-manifest.7c939af64826068b828e00ab4aa38356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc76eefce6f4042cecd77fa8ba3a1959",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88929c082f2b4184f05",
+    "url": "/static/css/main.35889a16.chunk.css"
+  },
+  {
+    "revision": "cc8a1672fced1c8eeee4",
+    "url": "/static/js/2.fb20c4fa.chunk.js"
+  },
+  {
+    "revision": "f88929c082f2b4184f05",
+    "url": "/static/js/main.677e87fb.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
